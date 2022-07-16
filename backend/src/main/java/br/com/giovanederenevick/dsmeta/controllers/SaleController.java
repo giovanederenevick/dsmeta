71f8dd2e58736c9e56548a13a01138bf93608d97
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2022-07-16 02:41:14 -0300

    Twilio SMS

--- backend/src/main/java/br/com/giovanederenevick/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/com/giovanederenevick/dsmeta/controllers/SaleController.java
@@ -1,14 +1,12 @@
 package br.com.giovanederenevick.dsmeta.controllers;
 
 import br.com.giovanederenevick.dsmeta.entities.Sale;
+import br.com.giovanederenevick.dsmeta.services.SMSService;
 import br.com.giovanederenevick.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +16,9 @@ public class SaleController {
 
     @Autowired
     private SaleService saleService;
+
+    @Autowired
+    private SMSService smsService;
 
     @GetMapping
     public Page<Sale> findSales(Pageable pageable) {
@@ -31,5 +32,11 @@ public class SaleController {
                                       Pageable pageable) {
 
         return saleService.findSalesByDate(minDate, maxDate, pageable);
+    }
+
+    @GetMapping(value = "/{id}/notification")
+    public void notifySMS(@PathVariable Long id) {
+
+        smsService.sendSms(id);
     }
 }
---
+++ backend/src/main/java/br/com/giovanederenevick/dsmeta/services/SMSService.java
@@ -0,0 +1,50 @@
+package br.com.giovanederenevick.dsmeta.services;
+
+import br.com.giovanederenevick.dsmeta.entities.Sale;
+import br.com.giovanederenevick.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SMSService {
+
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public void sendSms(Long saleId) {
+
+        Sale sale = saleRepository.findById(saleId).get();
+
+        StringBuffer msg = new StringBuffer();
+        msg.append("O vendedor ").append(sale.getSellerName());
+        msg.append(" foi destaque em ").append(sale.getDate().getMonthValue()).append("/").append(sale.getDate().getYear());
+        msg.append(" com um total de R$ ").append((String.format("%.2f", sale.getAmount())));
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Message message = Message.creator(to, from, msg.toString()).create();
+
+        log.info(message.getSid());
+    }
+}
