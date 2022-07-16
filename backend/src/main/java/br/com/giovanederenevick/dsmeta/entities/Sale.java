15d7e36f209223a9c594c64a4dcc17b0528ff71c
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2022-07-16 01:28:33 -0300

    Database

---
+++ backend/src/main/java/br/com/giovanederenevick/dsmeta/entities/Sale.java
@@ -0,0 +1,26 @@
+package br.com.giovanederenevick.dsmeta.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+}
