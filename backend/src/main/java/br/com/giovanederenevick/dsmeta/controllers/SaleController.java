4c21d40cc3a11dc22667a70733ea4d2f4c388040
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2022-07-16 02:02:13 -0300

    Date Select

--- backend/src/main/java/br/com/giovanederenevick/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/com/giovanederenevick/dsmeta/controllers/SaleController.java
@@ -3,8 +3,11 @@ package br.com.giovanederenevick.dsmeta.controllers;
 import br.com.giovanederenevick.dsmeta.entities.Sale;
 import br.com.giovanederenevick.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +20,16 @@ public class SaleController {
     private SaleService saleService;
 
     @GetMapping
-    public List<Sale> findSales() {
+    public Page<Sale> findSales(Pageable pageable) {
 
-        return saleService.findSales();
+        return saleService.findSales(pageable);
+    }
+
+    @GetMapping(value = "/bydate")
+    public Page<Sale> findSalesByDate(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                      @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+                                      Pageable pageable) {
+
+        return saleService.findSalesByDate(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/br/com/giovanederenevick/dsmeta/services/SaleService.java
+++ backend/src/main/java/br/com/giovanederenevick/dsmeta/services/SaleService.java
@@ -3,9 +3,13 @@ package br.com.giovanederenevick.dsmeta.services;
 import br.com.giovanederenevick.dsmeta.entities.Sale;
 import br.com.giovanederenevick.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -13,8 +17,18 @@ public class SaleService {
     @Autowired
     private SaleRepository saleRepository;
 
-    public List<Sale> findSales() {
+    public Page<Sale> findSales(Pageable pageable) {
 
-        return saleRepository.findAll();
+        return saleRepository.findAll(pageable);
+    }
+
+    public Page<Sale> findSalesByDate(String minDate, String maxDate, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = "".equals(minDate) ? today.minusYears(1) : LocalDate.parse(minDate);
+        LocalDate max = "".equals(maxDate) ? today : LocalDate.parse(maxDate);
+
+        return saleRepository.findSalesByDate(min, max, pageable);
     }
 }
